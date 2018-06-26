0063c08e8add95edc4e8ff9274482eebce51cf1c
Sheriffdeen Yusuf <sherifdeenmuhammed@gmail.com>
2018-06-26 13:29:48 +0100

    Adding Add Menu Icon for toolbar and more views

--- app/src/main/java/com/journalapp/MainActivity.java
+++ app/src/main/java/com/journalapp/MainActivity.java
@@ -3,7 +3,10 @@ package com.journalapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +18,24 @@ public class MainActivity extends AppCompatActivity {
     }
     private void setUpToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
-        if (getSupportActionBar() != null) {
+      //  if (getSupportActionBar() != null) {
             setSupportActionBar(toolbar);
+        //}
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_journal:
+                Toast.makeText(getApplicationContext(),"Add New Journal",Toast.LENGTH_SHORT).show();
+                break;
         }
+        return true;
     }
 }
