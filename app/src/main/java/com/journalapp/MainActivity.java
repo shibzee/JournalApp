0eb2d643ac1be3757976e9c049ebe30dc3e926ac
Sheriffdeen Yusuf <sherifdeenmuhammed@gmail.com>
2018-06-26 13:19:04 +0100

    some more basic views and toolbar setup

--- app/src/main/java/com/journalapp/MainActivity.java
+++ app/src/main/java/com/journalapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.journalapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpToolbar();
+    }
+    private void setUpToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        if (getSupportActionBar() != null) {
+            setSupportActionBar(toolbar);
+        }
     }
 }
